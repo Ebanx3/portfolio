c5a1f084686884218be44f3aafd05b0469dc2e59
Ebanx3 <esteban.dos.santos@gmail.com>
2024-11-26 21:41:29 -0300

    actions

--- js/checkServerStatus.js
+++ js/checkServerStatus.js
@@ -20,7 +20,7 @@ const showServerOff = () => {
   sendButton.classList.add("text-stone-700" , "border-stone-700");
 };
 
-fetch(`${secrets.SERVER_ADDRESS}/serverStatus`)
+fetch(`${SERVER_ADDRESS}/serverStatus`)
   .then((data) => data.json())
   .then((response) => {
     if (response.success) {
--- js/contactForm.js
+++ js/contactForm.js
@@ -21,7 +21,7 @@ contactForm.addEventListener("submit", (event) => {
   loadingDiv.classList.remove("hidden");
   loadingDiv.classList.add("flex");
 
-  fetch(`${secrets.SERVER_ADDRESS}/redirectEmail`, {
+  fetch(`${SERVER_ADDRESS}/redirectEmail`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, message }),
